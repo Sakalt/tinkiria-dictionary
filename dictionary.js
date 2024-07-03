88f6c1c4b6918a4841f0a9978ac04854a45f3049
Ingushe <167761978+Sakalt@users.noreply.github.com>
2024-07-03 17:21:28 +0900

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -28,5 +28,6 @@ const dictionaryData = [
     {"japanese": "キリン", "tisian": "kule", "definition": "大型の動物", "examples": ["kule が kafteを飲む。"], "type": "noun"},
     {"japanese": "辛い", "tisian": "petul", "definition": "味に刺激があること。", "examples": ["辛いカレー"], "type": "adjective"},
     {"japanese": "国", "tisian": "kint", "definition": "領土。", "examples": ["tisia kint"], "type": "noun"},
+    {"japanese": "虫", "tisian": "cuom", "definition": "骨を持たない生物。", "examples": ["dovekett sa cuom za 退治してくれる"], "type": "noun"},
     {"japanese": "象", "tisian": "klet", "definition": "大型の動物", "examples": ["象が水を飲む。"], "type": "noun"}
 ];
